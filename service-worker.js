cc8eb4e74b831c31196dd6d71b7e0e0e892bd953
sandzhi <sandji15012007@gmail.com>
2025-07-17 00:30:58 +0300

    Первоначальная загрузка PWA приложения
    Добавлены все файлы для Progressive Web App сравнения электротехники

---
+++ service-worker.js
@@ -0,0 +1,67 @@
+const CACHE_NAME = 'electrotech-cache-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/catalog.html',
+  '/compare.html',
+  '/feedback.html',
+  '/style.css',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  '/images/coil.jpg',
+  '/images/switch.jpg',
+  '/images/magnet.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Кэш успешно открыт');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then(response => {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            
+            return response;
+          })
+          .catch(() => {
+            return caches.match('/offline.html');
+          });
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
